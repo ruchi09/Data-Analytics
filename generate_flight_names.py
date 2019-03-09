c3738872ee7a5f44e5edcdd9682ca60f895925a8
ruchi09 <ruchisaha0909@gmail.com>
2019-03-09 23:52:14 +0530

    minor modifications

--- generate_flight_names.py
+++ generate_flight_names.py
@@ -1,27 +1,19 @@
 import random as r
 
-# place_names = ["Delhi","Bombay","Cape Town", "Dubai","Sri Lanka",  "London", "New York", "Indore", "Tokyo", "Sydney","Lucknow",
-#                 "Georgia","Beijing","California","Hong Kong","Shanghai"]
-
 
 if __name__ =='__main__':
     f=open("flight.csv","w")
 
     no_of_entries = 50
-    # max_entry = 15 #no of flights
-    # max_hop = 10 #no of hops
-    # min_hop = 7 #no of hops
-
-    # for _ in range(no_of_entries):
 
 
     fl = set()
     while len(fl)<no_of_entries:
         fl.add( r.randint(200,500))
 
-    # data = list()
+
     for i in fl:
-        a = r.randint(5,10)      # value*10^4
+        a = r.randint(5,10)      
         b = r.randint(9,13)
         c = "6E " + str(i) + "," + str(a) + "," + str(b) + "\n"
         f.write(c)
